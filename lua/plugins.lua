bc2c1a9ec9cb9584fc590055e4c985b21b99b6f1
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2023-07-22 01:30:36 +0300

    initial nvim config

---
+++ lua/plugins.lua
@@ -0,0 +1,55 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+    -- auto indentation
+    --'tpope/vim-sleuth',
+
+    {
+        'olimorris/onedarkpro.nvim',
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme 'onelight'
+        end
+    },
+
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        config = function() require("plugins.lualine") end
+    },
+
+    { 
+        'folke/which-key.nvim', 
+        opts = {} 
+    },
+
+    { 
+        'nvim-telescope/telescope.nvim', 
+        branch = '0.1.x', 
+        dependencies = { 'nvim-lua/plenary.nvim' }, 
+        config = function() require("plugins.telescope") end
+    },
+
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+            return vim.fn.executable 'make' == 1
+        end,
+    },
+})
+
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,5 @@
+require('telescope').setup {}
+
+pcall(require('telescope').load_extension, 'fzf')
+
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
